81fa3ab85f118ef98653692c460be22d0c9639d5
Getzyway <surawut.sod@gmail.com>
2018-07-08 15:20:48 +0700

    Intitial Laravl 5.6

---
+++ app/Http/Controllers/Admin/UsersController.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+//use App\User;
+use App\User as UserMod;
+use App\Model\Shop as ShopMod;
+use App\Model\Product as ProductMod;
+
+class UsersController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $mods = UserMod::paginate(5);
+        return view('admin.user.lists', compact('mods') );
+
+
+
+
+
+        /*$shop = ProductMod::find(6)->shop;
+        echo "<br /> Product is belongs to Shop :".$shop->name;*/
+
+        /*$shop = ShopMod::find(1);
+        echo "<br /> Shop :".$shop->name;
+        echo "<br /> Shop is belongs to :".$shop->user->name;*/
+
+        /*$products = ShopMod::find(1)->products;
+        //dd($products);
+
+        foreach ($products as $item) {
+            //dd($item); exit;
+            echo "<br /> Product :".$item->name." : ".$item->desc;
+        }*/
+
+
+        /*$user = UserMod::find(1);
+        echo "<br /> User :".$user->name;
+
+        $shop = UserMod::find(1)->shop;
+        echo "<br /> Shop name :".$shop->name;
+
+        $user = UserMod::find(1);
+        echo "<br /> Shop name2 :".$user->shop->name;*/
+
+
+
+        /*$mods = UserMod::where('active', 1)
+               ->where('city', 'bangkok')
+               ->where('name', 'like', '%user2%')
+               ->get();
+
+        foreach ($mods as $item) {
+            echo $item->name." : ".$item->email."<br />";
+        }*/
+
+        /*$mod = UserMod::find(1);
+        echo "Name :".$mod->name."<br /><br />";
+
+        $mods = UserMod::find([1,3,4]);
+        foreach ($mods as $item) {
+            echo $item->name." : ".$item->email."<br />";
+        }
+
+        $max = UserMod::where('active', 1)->max('age');
+        //echo "<br /> Age Max :".$max;*/
+
+        $count = UserMod::where('active', 1)->count();
+        //echo "<br /> Co :".$max;
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.user.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/demoone', 'DemoController@index');
+Route::post('/demotwo', 'DemoController@demotwo');
+
+Route::match(['get', 'post', 'delete'], '/demothree', 'DemoController@demothree');
+
+Route::any('/demofour', 'DemoController@demofour');
+
+Route::get('demosix/{id}/{name}', function ($id, $name) {
+    return 'ID: '.$id.' || NAME: '.$name;
+});
+
+Route::prefix('admin')->group(function () {
+    Route::any('demofour', 'DemoController@demofour');
+    Route::get('demoone', 'DemoController@index');
+});
+
+Route::get('demoten/age/{age}/school/{school}', function ($age, $school) {
+    return 'demoten age: '.$age.' || SCHOOL: '.$school;
+});
+
+Route::resource('photos', 'PhotoController');
+Route::resource('admin/users', 'Admin\UsersController');
